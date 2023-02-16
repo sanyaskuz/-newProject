8faffcf11ec6be1b3221f65ea415a89390c5c0a8
Oleksandr Skuz <oleksandr.skuz@gmail.com>
2023-02-16 15:14:14 +0200

    first-commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+
+namespace newProject {
+
+
+  class Program {
+
+    static  void Main() {
+      //Типи данних
+      // int num_1 = 10;
+      // byte num_2 = 15;
+      // short num_3 = 321;
+      // float num_4 = 3.24242f;
+      // string str = "Hello";
+      // bool bol = false;
+      // char s = 's';
+      ////////////
+      //зипис данних через консоль
+      // int num_1 = 0, num_2 = 0;
+
+      // num_1 = Convert.ToInt32(Console.ReadLine());
+      // num_2 = Convert.ToInt32(Console.ReadLine());
+     
+      // Console.WriteLine(num_1 + " " + num_2);
+      ///////////
+      // 
+      // Console.Write("Введіть число з крапкою: ");
+      // float user_input = float.Parse(Console.ReadLine());
+      
+      // Вычисление
+      // float result;
+      // result = user_input - 10f + 3.4f;
+      // result = user_input + 10f;
+      // result = user_input * 10f;
+      // result = user_input / 10f;
+      // result = user_input % 10f;
+
+      // int a = 5, b = 6;
+      // int res = a + b;
+      
+      //  int a = 5, b = 6;
+      // int res = a - b;
+
+      //  int a = 5, b = 6;
+      // int res = a * b;
+
+      //  float a = 5f, b = 6f;
+      // float res = a / b;
+
+      //  int a = 5, b = 6;
+      // int res = a % b;
+
+
+      //Mat funtction
+      //  Console.WriteLine(Math.Abs(-20));
+      // Console.WriteLine(Math.Ceiling(4.11f));
+      // Console.WriteLine(Math.Floor(4.99f));
+      // Console.WriteLine(Math.Round(4.51f));
+        //Максымальное ы мынымально 
+      // Console.WriteLine(Math.Min(0, 5));
+      // Console.WriteLine(Math.Max(0, 3));
+      //степень
+      // Console.WriteLine(Math.Pow(5, 3));
+
+      //Радиус круга
+      // System.Console.WriteLine("Введите радиус круга:");
+      // double radius = Convert.ToDouble(Console.ReadLine());
+      // double area = Math.PI * Math.Pow(radius, 2);
+      // System.Console.WriteLine("площадь круга с рфдусом {0} равно {1}", radius, area);
+
+      // 
+
+
+
+
+
+
+    }
+  }
+} 
